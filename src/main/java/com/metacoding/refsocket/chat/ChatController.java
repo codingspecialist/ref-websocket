83d9e6c85505a69ad4b027b6991eaf83c1c56220
getinthere <getinthere@naver.com>
2024-12-17 12:45:44 +0900

    websocket 완료

--- src/main/java/com/metacoding/refsocket/chat/ChatController.java
+++ src/main/java/com/metacoding/refsocket/chat/ChatController.java
@@ -2,6 +2,9 @@ package com.metacoding.refsocket.chat;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ChatController {
     private final ChatService chatService;
+    private final SimpMessageSendingOperations sms;
 
     @GetMapping("/save-form")
     public String saveForm(){
@@ -25,9 +29,26 @@ public class ChatController {
 
     @PostMapping("/chat")
     public String save(String msg){
-        chatService.save(msg);
+        Chat chat = chatService.save(msg);
+        sms.convertAndSend("/sub/chat", chat);
         return "redirect:/";
     }
+
+    // /pub/room
+//    @MessageMapping("/room")
+//    public void pubTest1(String number){
+//        System.out.println("나 요청돼? : "+ number);
+//        sms.convertAndSend("/sub/"+number, "hello world "+number);
+//    }
+
+//    @SendTo("/sub")
+//    @MessageMapping("/room")
+//    public String pubTest2(String number){
+//        System.out.println("나 요청돼? : "+ number);
+//        return "hello world";
+//    }
+
+
 }
 
 
